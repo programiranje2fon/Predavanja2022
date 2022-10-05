fcb040d32faf7cfe9705456097c5156986d4f807
Zoran Sevarac <zoran.sevarac@fon.bg.ac.rs>
2022-10-05 10:39:20 +0200

    Dodati komentari koda

--- src/dvocas1_ponedeljak/racun/PrimerRacuna.java
+++ src/dvocas1_ponedeljak/racun/PrimerRacuna.java
@@ -5,6 +5,7 @@ public class PrimerRacuna {
 	public static void main(String[] args) {
 		Racun mojRacun = new Racun(); // kreiranje objekta klase Racun sa new
 
+		// ucitavanje vrednosti i dodeljivanje atributima racuna
 		mojRacun.imeArtikla1 = Konzola.ucitajString("Unesite ime prvog artikla :"); // "Plazma keks";
 		mojRacun.cena1 = Konzola.ucitajDecimalniBroj("Unesite cenu prvog artikla:");
 		mojRacun.kolicina1 = Konzola.ucitajCeoBroj("Unesite kolicinu prvog artikla: ");
@@ -13,18 +14,21 @@ public class PrimerRacuna {
 		mojRacun.cena2 = Konzola.ucitajDecimalniBroj("Unesite cenu drugog artikla:");
 		mojRacun.kolicina2 = Konzola.ucitajCeoBroj("Unesite kolicinu drugog artikla: ");
 		
-		mojRacun.prikazi();
-		
-		Racun mojRacun2 = new Racun();
-		mojRacun2.prikazi();
 		
 		/*
+		// prikaz racuna 
 		System.out.println("Racun: ");
 		System.out.println(mojRacun.imeArtikla1 + " Cena: "+mojRacun.cena1 + " Kol: "+mojRacun.kolicina1);
 		System.out.println(mojRacun.imeArtikla2 + " Cena: "+mojRacun.cena2 + " Kol: "+mojRacun.kolicina2);
 		System.out.println("Iznos:" + mojRacun.izracunajUkupniIznos());
-		*/
+		*/		
 		
+		// prikaz racuna pomocu metode prikazi
+		mojRacun.prikazi();
+		
+		// kreiranje i prikaz drugog racuna
+		Racun mojRacun2 = new Racun();
+		mojRacun2.prikazi();		
 		
 	}
 
--- src/dvocas1_ponedeljak/racun/Racun.java
+++ src/dvocas1_ponedeljak/racun/Racun.java
@@ -1,12 +1,19 @@
 package dvocas1_ponedeljak.racun;
 
+/**
+ * Primer klase koju programer definise.
+ * class <NazivKlase> {
+ * 
+ * } 
+ */
 public class Racun {
-	static int brojRacuna = 0;
+	static int brojRacuna = 0; 	// staticki atribut  - zajednicki za sve objekte ove klase
+
 	String imeArtikla1; // atribut - promenljiva pridruzena klasi
 	double cena1;
 	int kolicina1;
 	
-	String imeArtikla2; // atribut - promenljiva pridruzena klasi
+	String imeArtikla2;
 	double cena2;
 	int kolicina2;
 	
@@ -14,17 +21,27 @@ public class Racun {
 	String prodavnica;
 	String qrKod;
 	
-	
+	/**
+	 * Konstruktor - specijalni metod koji se poziva prilikom kreiranja 
+	 * objekta i vrsi pocetnu inmicijalizaciju objekta.
+	 */
 	Racun() {
 		brojRacuna++;
 	}
 	
 	
+	/**
+	 * Metoda koja izracunava i vraca ukupan iznos racuna
+	 * @return ukupan iznos racuna
+	 */
 	double izracunajUkupniIznos() {
 		double iznos = cena1*kolicina1 + cena2*kolicina2;
 		return iznos;
 	}
 	
+	/**
+	 * Metoda koja ispisuje na ekranu sve podatke o ovom racunu
+	 */
 	void prikazi() {
 		System.out.println("Racun br: "+brojRacuna);
 		System.out.println(imeArtikla1 + " Cena: "+cena1 + " Kol: "+kolicina1);
@@ -35,6 +52,10 @@ public class Racun {
 		
 	}
 	
+	/**
+	 * Metoda koja izracunava i vraca PDV za ovaj racun.
+	 * @return porez (PDV) na ukupan izos ovog racuna
+	 */
 	double izracunajPDV() {
 		double pdv = izracunajUkupniIznos()*0.2;
 		return pdv;
