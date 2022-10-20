80a39fdce05857c85235e2b0b01c504054c39818
bojantomic <tomicb@fon.rs>
2022-10-20 10:53:16 +0200

    Dodata resenja za dvocas 3 sreda

---
+++ src/dvocas3_sreda/zadatak1/TestIspisivac.java
@@ -0,0 +1,21 @@
+package dvocas3_sreda.zadatak1;
+
+public class TestIspisivac {
+
+	public static void main(String[] args) {
+		//Ispisivac is = new Ispisivac();
+		
+		//Ispisivac.ispisiOcenu(5);
+		
+		//Ispisivac.ispisiZdravo();
+		
+		//Ispisivac.ispisiPoruku("Poruka1");
+		
+		//Ispisivac.ispisiPoruku("Poruka2", 3);
+		
+		//Ispisivac.ispisiBrojeveOd1Do20();
+		
+		Ispisivac.vremeDupliranja(1000, 10);
+	}
+
+}
