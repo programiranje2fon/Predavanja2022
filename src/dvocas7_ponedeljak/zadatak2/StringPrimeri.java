251aeafbd372e10cc51145ffd9f466a91976f307
Zoran Sevarac <zoran.sevarac@fon.bg.ac.rs>
2022-11-14 14:06:50 +0100

    zad 7

---
+++ src/dvocas7_ponedeljak/zadatak2/StringPrimeri.java
@@ -0,0 +1,15 @@
+package dvocas7_ponedeljak.zadatak2;
+
+public class StringPrimeri {
+
+	public static void main(String[] args) {
+		String tekst = "Neki tekst";
+		
+		int indeksSlovaT = tekst.indexOf(':');
+		
+		int poslednjiIndeksSlovaT = tekst.lastIndexOf('t');
+		System.out.println(indeksSlovaT);
+
+	}
+
+}
